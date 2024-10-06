eed743ae5973f4edf6102a835700edf2a2b0a305
Soumithra Debnath Shanto <soumithradebnathshanto@gmail.com>
2024-10-06 11:27:04 +0600

    first commit

---
+++ Stack/infixToPostfix.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int precedence(char ope) {
+    if (ope == '+' || ope == '-') return 1;
+    if (ope == '*' || ope == '/') return 2;
+    if (ope == '^') return 3; // Higher precedence for exponentiation
+    return 0;
+}
+
+string infixToPostfix(string infix){
+    stack<char>ope;
+    string postfix = "";
+    for(int i = 0; i < infix.size(); i++){
+        if(infix[i] == '('){
+            ope.push(infix[i]);
+        }else if((infix[i] >= 'a' && infix[i] <= 'z') || (infix[i] >= 'A' && infix[i] <= 'Z') || (infix[i] >= '0' && infix[i] <= '9')){
+            postfix += infix[i];
+        }else if(infix[i] == ')'){
+            while(ope.top() != '('){
+                postfix += ope.top();
+                ope.pop();
+            }
+            ope.pop();
+        }else{
+            ope.push(infix[i]);
+        }
+    }
+    while(!ope.empty()){
+        postfix += ope.top();
+        ope.pop();
+    }
+    return postfix;
+}
+
+int main() {
+    string str = "(A+2)*(3^B)";
+    cout << infixToPostfix(str) << "\n";
+    
+    
+    return 0;
+}
